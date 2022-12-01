92a8c7c972e46db3f09e93e3e3a7b74400e11868
Yan BUATOIS <yanbuatois@outlook.fr>
2022-12-01 12:30:32 +0100

    feat: added first day participation

---
+++ 01/01.js
@@ -0,0 +1,52 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import 'colors';
+
+console.time('main');
+console.time('init');
+const calories = fs
+	.readFileSync(path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'input'), {
+		encoding: 'utf-8',
+	})
+	.trim()
+	.split('\n')
+	.map((line) => line.trim())
+	.reduce(
+		(acc, row) => {
+			if (!row) {
+				acc.push(0);
+			} else {
+				acc[acc.length - 1] += +row;
+			}
+			return acc;
+		},
+		[0]
+	);
+
+console.timeEnd('init');
+
+console.time('part 1');
+const maxCalories = Math.max(...calories);
+
+console.log(`The maximum calories is ${maxCalories.toString().red} !`);
+
+console.timeEnd('part 1');
+console.time('part 2');
+function nbMaxOfArray(array, number = 3) {
+	if (array.length <= number) {
+		return Array.from(array);
+	} else {
+		return nbMaxOfArray(
+			array.filter((elt, index) => index !== array.indexOf(Math.min(...array)), 1),
+			number
+		);
+	}
+}
+const threeMaxCalories = nbMaxOfArray(calories);
+const threeMaxCaloriesSum = threeMaxCalories.reduce((acc, value) => acc + value);
+
+console.log(`The sum of the three max calories is ${threeMaxCaloriesSum.toString().green} !`);
+console.timeEnd('part 2');
+console.timeEnd('main');
